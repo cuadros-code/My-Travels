8d85838b36fa9ee4ee9a4c4355abb908471cef81
Kevin Cuadros <kevindavidcuadros@gmail.com>
2023-04-14 23:56:42 -0500

    image background

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -12,68 +12,70 @@ export default function Auth() {
   }
   
   return (
-    <div class={`${styles.container} ${ styles[style()] }`}>
-      <div class={`${styles['form-container']} ${styles['sign-up-container']}`}>
-          <form action="#">
-              <h1 class={styles.title}>Creá tu cuenta</h1>
-              <input type="text" placeholder="Nombre completo" />
-              <input type="email" placeholder="Correo" />
-              <input type="password" placeholder="Contraseña" />
-              <Button>Crear cuenta</Button>
-              <Button class={styles.social} >
-                {
-                  authStore.loading
-                  ? <LoadingIndicator></LoadingIndicator>
-                  : <>
-                      <img src="google.svg" alt="" />
-                      <span>
-                        Regístrate con Google
-                      </span>
-                    </>   
-                }
-              </Button>
-          </form>
-      </div>
-      <div class={`${styles['form-container']} ${styles['log-in-container']}`}>
-          <form action="#">
-              <h1 class={styles.title}>Bienvenido</h1>
-              <input type="email" placeholder="Correo" />
-              <input type="password" placeholder="Contraseña" />
-              <a class={styles.link} href="#">¿Olvidaste tu contraseña?</a>
-              <Button>Iniciar Sesión</Button>
-              <Button 
-                class={styles.social} 
-                onclick={onSignInWithGoogle}
-              >
-                <>
+    <div class={styles.content}>
+      <div class={`${styles.container} ${ styles[style()] }`}>
+        <div class={`${styles['form-container']} ${styles['sign-up-container']}`}>
+            <form action="#">
+                <h1 class={styles.title}>Creá tu cuenta</h1>
+                <input type="text" placeholder="Nombre completo" />
+                <input type="email" placeholder="Correo" />
+                <input type="password" placeholder="Contraseña" />
+                <Button>Crear cuenta</Button>
+                <Button class={styles.social} >
                   {
-                    authStore.loading 
+                    authStore.loading
                     ? <LoadingIndicator></LoadingIndicator>
                     : <>
                         <img src="google.svg" alt="" />
                         <span>
-                          Inicia sesión con Google
+                          Regístrate con Google
                         </span>
-                      </>
+                      </>   
                   }
-                </>
+                </Button>
+            </form>
+        </div>
+        <div class={`${styles['form-container']} ${styles['log-in-container']}`}>
+            <form action="#">
+                <h1 class={styles.title}>Bienvenido</h1>
+                <input type="email" placeholder="Correo" />
+                <input type="password" placeholder="Contraseña" />
+                <a class={styles.link} href="#">¿Olvidaste tu contraseña?</a>
+                <Button>Iniciar Sesión</Button>
+                <Button 
+                  class={styles.social} 
+                  onclick={onSignInWithGoogle}
+                >
+                  <>
+                    {
+                      authStore.loading 
+                      ? <LoadingIndicator></LoadingIndicator>
+                      : <>
+                          <img src="google.svg" alt="" />
+                          <span>
+                            Inicia sesión con Google
+                          </span>
+                        </>
+                    }
+                  </>
 
-              </Button>
-          </form>
-      </div>
-      <div class={styles['overlay-container']}>
-          <div class={styles['overlay']}>
-              <div class={`${styles['overlay-panel']} ${styles['overlay-left']}`}>
-                  <h1>Welcome Back!</h1>
-                  <p>Already have an account? Log In</p>
-                  <Button onclick={() => setStyle('')} class={styles.ghost} id="logIn">Log In</Button>
-              </div>
-              <div class={`${styles['overlay-panel']} ${styles['overlay-right']}`}>
-                  <h1>Hello, There!</h1>
-                  <p>Don't have an account? Sign Up Free</p>
-                  <Button onclick={() => setStyle('right-panel-active')} class={styles.ghost} id="signUp">Sign Up</Button>
-              </div>
-          </div>
+                </Button>
+            </form>
+        </div>
+        <div class={styles['overlay-container']}>
+            <div class={styles['overlay']}>
+                <div class={`${styles['overlay-panel']} ${styles['overlay-left']}`}>
+                    <h1>Welcome Back!</h1>
+                    <p>Already have an account? Log In</p>
+                    <Button onclick={() => setStyle('')} class={styles.ghost} id="logIn">Log In</Button>
+                </div>
+                <div class={`${styles['overlay-panel']} ${styles['overlay-right']}`}>
+                    <h1>Hello, There!</h1>
+                    <p>Don't have an account? Sign Up Free</p>
+                    <Button onclick={() => setStyle('right-panel-active')} class={styles.ghost} id="signUp">Sign Up</Button>
+                </div>
+            </div>
+        </div>
       </div>
     </div>
   );
